3d42440e420ca1f6871b8ac436e05b10e4c32995
Mark Slee <mcslee@apache.org>
2007-02-21 04:17:34 +0000

    Thrift application exceptions in Java
    Reviewed By: thrift
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665008 13f79535-47bb-0310-9956-ffa450edef68

--- lib/java/src/protocol/TMessageType.java
+++ lib/java/src/protocol/TMessageType.java
@@ -8,4 +8,5 @@ package com.facebook.thrift.protocol;
 public final class TMessageType {
   public static final byte CALL  = 1;
   public static final byte REPLY = 2;
+  public static final byte EXCEPTION = 3;
 }
