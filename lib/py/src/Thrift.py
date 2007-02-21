e74306a96556748f887628deddec544fe24b7be9
Mark Slee <mcslee@apache.org>
2007-02-21 05:38:12 +0000

    Move constant defs into Thrift.py to avoid cirulcar includes
    Reviewed By: snakes
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665016 13f79535-47bb-0310-9956-ffa450edef68

--- lib/py/src/Thrift.py
+++ lib/py/src/Thrift.py
@@ -1,3 +1,27 @@
+class TType:
+  STOP   = 0
+  VOID   = 1
+  BOOL   = 2
+  BYTE   = 3
+  I08    = 3
+  DOUBLE = 4
+  I16    = 6
+  I32    = 8
+  I64    = 10
+  STRING = 11
+  UTF7   = 11
+  STRUCT = 12
+  MAP    = 13
+  SET    = 14
+  LIST   = 15
+  UTF8   = 16
+  UTF16  = 17
+
+class TMessageType:
+  CALL  = 1
+  REPLY = 2
+  EXCEPTION = 3
+
 class TProcessor:
 
   """Base class for procsessor, which works on two streams."""
