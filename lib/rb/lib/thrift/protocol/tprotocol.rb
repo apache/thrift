43df5c5b8a942f59b28c04e7091e4db0a39bd1e3
Mark Slee <mcslee@apache.org>
2007-03-01 00:34:42 +0000

    Applying Thrift license to ruby source
    Reviewed By: ruby
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665034 13f79535-47bb-0310-9956-ffa450edef68

--- lib/rb/lib/thrift/protocol/tprotocol.rb
+++ lib/rb/lib/thrift/protocol/tprotocol.rb
@@ -1,3 +1,13 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2006- Facebook
+# Distributed under the Thrift Software License
+#
+# See accompanying file LICENSE or visit the Thrift site at:
+# http://developers.facebook.com/thrift/
+#
+# Author: Mark Slee <mcslee@facebook.com>
+#
 class TType
   STOP = 0
   VOID = 1
