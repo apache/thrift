ba8f8d7953c06e27bac7f569d628a49c27231c73
Mark Slee <mcslee@apache.org>
2007-04-03 00:34:00 +0000

    Remove extra TApplicationException:: in Thrift.h
    Summary: G++ 4.1.1 compatibility
    
    Reviewed By: alex tribble
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665078 13f79535-47bb-0310-9956-ffa450edef68

--- lib/cpp/src/Thrift.h
+++ lib/cpp/src/Thrift.h
@@ -98,8 +98,8 @@ public:
     }
   }
 
-  uint32_t TApplicationException::read(protocol::TProtocol* iprot);
-  uint32_t TApplicationException::write(protocol::TProtocol* oprot) const;
+  uint32_t read(protocol::TProtocol* iprot);
+  uint32_t write(protocol::TProtocol* oprot) const;
 
 protected:
   /**
