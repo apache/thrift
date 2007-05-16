e9ce01cf402fca4c275554847c85aff9fff17dc6
Mark Slee <mcslee@apache.org>
2007-05-16 02:29:53 +0000

    Apply thrift license to all files in compiler
    Summary: Forgot to do this last time around, only got the libs
    
    Reviewed By: no code in here, just comments
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665113 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/parse/t_doc.h
+++ compiler/cpp/src/parse/t_doc.h
@@ -1,31 +1,36 @@
+// Copyright (c) 2006- Facebook
+// Distributed under the Thrift Software License
+//
+// See accompanying file LICENSE or visit the Thrift site at:
+// http://developers.facebook.com/thrift/
+
 #ifndef T_DOC_H
 #define T_DOC_H
 
 /**
- *
+ * Documentation stubs
  *
  * @author Charlie Cheever <charlie@facebook.com>
  */
 class t_doc {
 
-  public:
-
-    void set_doc(const std::string& doc) {
-      doc_ = doc;
-      has_doc_ = true;
-    }
-
-    const std::string& get_doc() const {
-      return doc_;
-    }
-
-    bool has_doc() {
-      return has_doc_;
-    }
-
-  private:
-    std::string doc_;
-    bool has_doc_;
+ public:
+  void set_doc(const std::string& doc) {
+    doc_ = doc;
+    has_doc_ = true;
+  }
+  
+  const std::string& get_doc() const {
+    return doc_;
+  }
+  
+  bool has_doc() {
+    return has_doc_;
+  }
+  
+ private:
+  std::string doc_;
+  bool has_doc_;
 
 };
 
--- compiler/cpp/src/thriftl.ll
+++ compiler/cpp/src/thriftl.ll
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) 2006- Facebook
+ * Distributed under the Thrift Software License
+ *
+ * See accompanying file LICENSE or visit the Thrift site at:
+ * http://developers.facebook.com/thrift/
+ */
+
+/**
  * Thrift scanner.
  * 
  * Tokenizes a thrift definition file.
