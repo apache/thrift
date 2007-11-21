7962223ce299bbeb20348b3819a423a72efa2335
Mark Slee <mcslee@apache.org>
2007-11-21 23:35:52 +0000

    libtoolize and configure tweaks for Thrift compiler
    Summary: Need glibtoolize on OSX.
    
    Reviewed By: mcslee
    
    Other Notes: From Paul Collison
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665363 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/bootstrap.sh
+++ compiler/cpp/bootstrap.sh
@@ -3,7 +3,11 @@
 ./cleanup.sh
 autoscan
 aclocal -I ../../lib/cpp/aclocal
-libtoolize --automake
+if libtoolize --version 1 >/dev/null 2>/dev/null; then
+  libtoolize --automake
+elif glibtoolize --version 1 >/dev/null 2>/dev/null; then
+  glibtoolize --automake
+fi
 touch NEWS README AUTHORS ChangeLog
 autoconf
 automake -ac
