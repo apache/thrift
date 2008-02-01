84e5022bfc9a815ce5f14e6effd980c6672da872
dweatherford <dev-null@apache.org>
2008-02-01 20:42:56 +0000

    [thrift] Fix generated T_LIST push_back() for cpptype override
    Summary: Thrift compiler emitting code missing a closing paren:
      "this->extras.push_back(_elem18;"
    Reviewed by: eletuchy
    Test plan: rebuild some feed code that uses cpptype overrides
    Revert: ok
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@665453 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/generate/t_cpp_generator.cc
+++ compiler/cpp/src/generate/t_cpp_generator.cc
@@ -2349,7 +2349,7 @@ void t_cpp_generator::generate_deserialize_list_element(ofstream& out,
     t_field felem(tlist->get_elem_type(), elem);
     indent(out) << declare_field(&felem) << endl;
     generate_deserialize_field(out, &felem);
-    indent(out) << prefix << ".push_back(" << elem << ";" << endl;
+    indent(out) << prefix << ".push_back(" << elem << ");" << endl;
   } else {
     t_field felem(tlist->get_elem_type(), prefix + "[" + index + "]");
     generate_deserialize_field(out, &felem);
