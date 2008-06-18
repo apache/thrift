5e44bce98f289a196d2df0ec0d91fc1f9a3302be
Kevin Clark <kclark@apache.org>
2008-06-18 00:52:46 +0000

    rb: Remove extra return statement
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@668900 13f79535-47bb-0310-9956-ffa450edef68

--- lib/rb/lib/thrift/deprecation.rb
+++ lib/rb/lib/thrift/deprecation.rb
@@ -5,7 +5,7 @@ class Module
          alias_method :deprecated_#{method_name}, :#{method_name}
          def #{method_name}(*args, &block)
            $stderr.puts "Warning: calling deprecated method: #{self}.#{method_name}"
-           return deprecated_#{method_name}(*args, &block)
+           deprecated_#{method_name}(*args, &block)
          end
        END
       end
