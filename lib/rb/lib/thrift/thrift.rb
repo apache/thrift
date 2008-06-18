6fda4b76e2c8a963fcc28edac725da48fffd1784
Kevin Clark <kclark@apache.org>
2008-06-18 00:57:26 +0000

    Move thrift/protocol/tprotocol to thrift/protocol and thrift/protocol/tbinaryprotocol to thrift/protocol/binaryprotocol. Leave shim files behind for backwards compatibility
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@668914 13f79535-47bb-0310-9956-ffa450edef68

--- lib/rb/lib/thrift/thrift.rb
+++ lib/rb/lib/thrift/thrift.rb
@@ -1,8 +1,9 @@
 # This file kept for backwards compatability
 # require File.join(File.dirname(__FILE__), '../thrift')
-require File.join(File.dirname(__FILE__), 'deprecation')
-require File.join(File.dirname(__FILE__), 'types')
-require File.join(File.dirname(__FILE__), 'processor')
-require File.join(File.dirname(__FILE__), 'exceptions')
-require File.join(File.dirname(__FILE__), 'client')
-require File.join(File.dirname(__FILE__), 'struct')
+$:.unshift File.dirname(File.dirname(__FILE__))
+require 'thrift/deprecation'
+require 'thrift/types'
+require 'thrift/processor'
+require 'thrift/exceptions'
+require 'thrift/client'
+require 'thrift/struct'
