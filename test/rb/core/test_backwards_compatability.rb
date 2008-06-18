dd8ef9ef53c3cf4da28e641d05a16b65af0c4bcc
Kevin Clark <kclark@apache.org>
2008-06-18 01:03:48 +0000

    Remove test_remappings because it isn't comprehensive and it's triggering undesired warnings. This should be re-created as a spec.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@668936 13f79535-47bb-0310-9956-ffa450edef68

--- test/rb/core/test_backwards_compatability.rb
+++ test/rb/core/test_backwards_compatability.rb
@@ -5,28 +5,8 @@ require 'thrift/thrift'
 class TestTException < Test::Unit::TestCase
   def test_has_accessible_message
     msg = "hi there thrift"
-    assert_equal msg, TException.new(msg).message
+    assert_equal msg, Thrift::Exception.new(msg).message
   end
   
-end
-
-class TestConstRemapping < Test::Unit::TestCase
-  def test_remappings
-    maps = {
-      TException => Thrift::Exception,
-      TApplicationException => Thrift::ApplicationException,
-      TType => Thrift::Types,
-      TMessageType => Thrift::MessageTypes,
-      TProcessor => Thrift::Processor,
-      ThriftClient => Thrift::Client,
-      ThriftStruct => Thrift::Struct,
-      TProtocol => Thrift::Protocol,
-      TProtocolException => Thrift::ProtocolException
-    }
-    
-    maps.each do |k, v|
-      assert_equal k, v
-    end
-  end
 end
 
