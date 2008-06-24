c5336ff6915c454b7f10c6fe473e2257a1303367
Kevin Clark <kclark@apache.org>
2008-06-24 01:06:06 +0000

    rb: Change the license from Thrift Software License to Apache Software License [THRIFT-38]
    From kevin@rapleaf.com
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@670983 13f79535-47bb-0310-9956-ffa450edef68

--- lib/rb/lib/thrift.rb
+++ lib/rb/lib/thrift.rb
@@ -1,6 +1,6 @@
 #
 # Copyright (c) 2006- Facebook
-# Distributed under the Thrift Software License
+# Distributed under the Apache Software License
 #
 # See accompanying file LICENSE or visit the Thrift site at:
 # http://developers.facebook.com/thrift/
