6341d13a1f87c3426965bd51cba6bf331ed0c36b
Mark Slee <mcslee@apache.org>
2008-09-29 22:01:30 +0000

    Python generator exception namespacing, THRIFT-150
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@700277 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/generate/t_py_generator.cc
+++ compiler/cpp/src/generate/t_py_generator.cc
@@ -1355,7 +1355,7 @@ void t_py_generator::generate_process_function(t_service* tservice,
     indent_down();
     for (x_iter = xceptions.begin(); x_iter != xceptions.end(); ++x_iter) {
       f_service_ <<
-        indent() << "except " << (*x_iter)->get_type()->get_name() << ", " << (*x_iter)->get_name() << ":" << endl;
+        indent() << "except " << type_name((*x_iter)->get_type()) << ", " << (*x_iter)->get_name() << ":" << endl;
       if (!tfunction->is_async()) {
         indent_up();
         f_service_ <<
