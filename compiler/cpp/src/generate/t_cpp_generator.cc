66f5e9ba0bd9e4605c48913e29bd6bed79f5a579
David Reiss <dreiss@apache.org>
2008-10-22 22:07:03 +0000

    Make a pointer const to fix a warning
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@707213 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/generate/t_cpp_generator.cc
+++ compiler/cpp/src/generate/t_cpp_generator.cc
@@ -823,7 +823,7 @@ void t_cpp_generator::generate_struct_fingerprint(ofstream& out,
         tstruct->get_ascii_fingerprint() << "\";" << endl <<
       indent() << stat << "const uint8_t " << nspace <<
         "binary_fingerprint[" << t_type::fingerprint_len << "]" << comment << "= {";
-    char* comma = "";
+    const char* comma = "";
     for (int i = 0; i < t_type::fingerprint_len; i++) {
       out << comma << "0x" << t_struct::byte_to_hex(tstruct->get_binary_fingerprint()[i]);
       comma = ",";
