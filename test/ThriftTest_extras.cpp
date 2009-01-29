c04452eff2db9b5c524522f0dcf0b2e776115efd
David Reiss <dreiss@apache.org>
2009-01-29 23:46:46 +0000

    THRIFT-289. cpp: Fix "make check" by defining a comparator for "Instanity"
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@739083 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/ThriftTest_extras.cpp
@@ -0,0 +1,14 @@
+// Extra functions required for ThriftTest_types to work
+
+#include <protocol/TDebugProtocol.h>
+#include "gen-cpp/ThriftTest_types.h"
+
+
+namespace thrift { namespace test {
+
+bool Insanity::operator<(thrift::test::Insanity const& other) const {
+  using facebook::thrift::ThriftDebugString;
+  return ThriftDebugString(*this) < ThriftDebugString(other);
+}
+
+}}
