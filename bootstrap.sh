8db8980fa106d585bad706e503f7d9baa89cf7cb
David Reiss <dreiss@apache.org>
2009-05-06 22:00:33 +0000

    THRIFT-483. Use --copy for libtoolize
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@772440 13f79535-47bb-0310-9956-ffa450edef68

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,9 +26,9 @@ aclocal -I ./aclocal || exit 1
 autoheader || exit 1
 
 if libtoolize --version 1 >/dev/null 2>/dev/null; then
-  libtoolize --automake || exit 1
+  libtoolize --copy --automake || exit 1
 elif glibtoolize --version 1 >/dev/null 2>/dev/null; then
-  glibtoolize --automake || exit 1
+  glibtoolize --copy --automake || exit 1
 fi
 
 autoconf
