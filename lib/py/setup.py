16514d0a724f9a7ad4a3fe7a854335f949ba25fa
David Reiss <dreiss@apache.org>
2009-05-10 05:14:44 +0000

    THRIFT-494. python: Update meta-information in setup.py
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@773306 13f79535-47bb-0310-9956-ffa450edef68

--- lib/py/setup.py
+++ lib/py/setup.py
@@ -26,11 +26,12 @@ fastbinarymod = Extension('thrift.protocol.fastbinary',
                           )
 
 setup(name = 'Thrift',
-      version = '1.0',
+      version = '0.1',
       description = 'Thrift Python Libraries',
-      author = ['Mark Slee'],
-      author_email = ['mcslee@facebook.com'],
-      url = 'http://code.facebook.com/thrift',
+      author = ['Thrift Developers'],
+      author_email = ['thrift-dev@incubator.apache.org'],
+      url = 'http://incubator.apache.org/thrift/',
+      license = 'Apache License 2.0',
       packages = [
         'thrift',
         'thrift.protocol',
