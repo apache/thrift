96b2ccc0d4c9b19530d5dbd81643cff1ad6fb6c0
Bryan Duxbury <bryanduxbury@apache.org>
2010-09-24 16:47:59 +0000

    THRIFT-881. csharp: add csharp to the tutorial
    This patch adds a tutorial for CSharp. It also moves the 'test' client and server into the tutorial, because they were not being used by the tests.
    
    Patch: Roger Meier
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/thrift/trunk@1000953 13f79535-47bb-0310-9956-ffa450edef68

--- tutorial/csharp/CsharpClient/CsharpClient.cs
+++ tutorial/csharp/CsharpClient/CsharpClient.cs
@@ -46,9 +46,9 @@ namespace CSharpTutorial
 
                 Work work = new Work();
 
-                work.op = Operation.DIVIDE;
-                work.num1 = 1;
-                work.num2 = 0;
+                work.Op = Operation.DIVIDE;
+                work.Num1 = 1;
+                work.Num2 = 0;
                 try
                 {
                     int quotient = client.calculate(1, work);
@@ -56,12 +56,12 @@ namespace CSharpTutorial
                 }
                 catch (InvalidOperation io)
                 {
-                    Console.WriteLine("Invalid operation: " + io.why);
+                    Console.WriteLine("Invalid operation: " + io.Why);
                 }
 
-                work.op = Operation.SUBTRACT;
-                work.num1 = 15;
-                work.num2 = 10;
+                work.Op = Operation.SUBTRACT;
+                work.Num1 = 15;
+                work.Num2 = 10;
                 try
                 {
                     int diff = client.calculate(1, work);
@@ -69,11 +69,11 @@ namespace CSharpTutorial
                 }
                 catch (InvalidOperation io)
                 {
-                    Console.WriteLine("Invalid operation: " + io.why);
+                    Console.WriteLine("Invalid operation: " + io.Why);
                 }
 
                 SharedStruct log = client.getStruct(1);
-                Console.WriteLine("Check log: {0}", log.value);
+                Console.WriteLine("Check log: {0}", log.Value);
 
                 transport.Close();
             }
