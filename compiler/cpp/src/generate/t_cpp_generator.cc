cdbf36c6082a3931e41d5d1fb8ba8b5cb3eade23
Jake Farrell <jfarrell@apache.org>
2011-07-14 21:25:08 +0000

    Thrift-1233: Remove unused include in generated C++ code Client: c++ Patch: Diwaker Gupta
    Removing unused include of TTransportUtils.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1146901 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/generate/t_cpp_generator.cc
+++ compiler/cpp/src/generate/t_cpp_generator.cc
@@ -1483,8 +1483,6 @@ void t_cpp_generator::generate_service(t_service* tservice) {
   if (gen_cob_style_) {
     f_header_ <<
       "#include <tr1/functional>" << endl <<
-      // TODO(dreiss): Libify the base client so we don't have to include this.
-      "#include <transport/TTransportUtils.h>" << endl <<
       "namespace apache { namespace thrift { namespace async {" << endl <<
       "class TAsyncChannel;" << endl <<
       "}}}" << endl;
