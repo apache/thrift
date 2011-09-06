eb7a755af4cb983e130194985c7f54a742ece889
Jake Farrell <jfarrell@apache.org>
2011-09-06 16:26:58 +0000

    Thrift-1322: OCaml lib fail to compile: Thrift.ml line 305, int vs int32 mismatch Client: OCaml
    Updates the ocaml lib INTERNAL and PROTOCOL error enums to use the correct numeric values
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1165753 13f79535-47bb-0310-9956-ffa450edef68

--- lib/ocaml/src/Thrift.ml
+++ lib/ocaml/src/Thrift.ml
@@ -302,8 +302,8 @@ struct
     | 3l -> WRONG_METHOD_NAME
     | 4l -> BAD_SEQUENCE_ID
     | 5l -> MISSING_RESULT
-    | 61 -> INTERNAL_ERROR
-    | 71 -> PROTOCOL_ERROR
+    | 6l -> INTERNAL_ERROR
+    | 7l -> PROTOCOL_ERROR
     | _ -> raise Thrift_error;;
   let typ_to_i = function
     | UNKNOWN -> 0l
@@ -312,8 +312,8 @@ struct
     | WRONG_METHOD_NAME -> 3l
     | BAD_SEQUENCE_ID -> 4l
     | MISSING_RESULT -> 5l
-    | INTERNAL_ERROR -> 61
-    | PROTOCOL_ERROR -> 71
+    | INTERNAL_ERROR -> 6l
+    | PROTOCOL_ERROR -> 7l
 
   class t =
   object (self)
