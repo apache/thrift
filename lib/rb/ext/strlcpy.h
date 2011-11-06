9e7a88267c1a45d06ca3f332f683e2bbfa4eb9a0
Jake Farrell <jfarrell@apache.org>
2011-11-06 19:58:32 +0000

    Thrift-1382: Bundle install doesnot work because thrift crashes Client: ruby
    Fixing strlcpy header.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1198543 13f79535-47bb-0310-9956-ffa450edef68

--- lib/rb/ext/strlcpy.h
+++ lib/rb/ext/strlcpy.h
@@ -18,11 +18,12 @@
  */
 
 
+#include <sys/types.h>
 #include <string.h>
 
 #ifndef HAVE_STRLCPY
 size_t
-strlcpy (char *dst, const char *src, size_t dst_sz)
+strlcpy (char *dst, const char *src, size_t dst_sz);
 #else
 extern size_t strlcpy(char *, const char *, size_t);
 #endif
