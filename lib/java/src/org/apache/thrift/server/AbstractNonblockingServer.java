f3e48d4a2e0392d25b02fbfb2df51489744b250e
Jake Farrell <jfarrell@apache.org>
2011-12-14 23:48:22 +0000

    THRIFT-1464: AbstractNonblockingServer.FrameBuffer TNonblockingTransport accessor changed from public to private Client: java Patch: Jake Farrell
    Fixes accessor for TNonblockingTransport in AbstractNonblockingServer.FrameBuffer that was changed from public to private
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1214557 13f79535-47bb-0310-9956-ffa450edef68

--- lib/java/src/org/apache/thrift/server/AbstractNonblockingServer.java
+++ lib/java/src/org/apache/thrift/server/AbstractNonblockingServer.java
@@ -267,7 +267,7 @@ public abstract class AbstractNonblockingServer extends TServer {
    */
   protected class FrameBuffer {
     // the actual transport hooked up to the client.
-    private final TNonblockingTransport trans_;
+    public final TNonblockingTransport trans_;
 
     // the SelectionKey that corresponds to our transport
     private final SelectionKey selectionKey_;
