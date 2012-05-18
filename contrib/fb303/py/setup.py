eccbe4d08d6220567aefdc222332e1b74f30d0db
Jake Farrell <jfarrell@apache.org>
2012-05-18 03:57:07 +0000

    THRIFT-1539:Build and distribute the fb303 python libraries along with thrift Client: contrib/fb303 - py Patch: Jake Farrell
    Updated build file so dist egg will include project headers, will publish with py client lib to pypi as part of release process.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1339967 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/fb303/py/setup.py
+++ contrib/fb303/py/setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements. See the NOTICE file
@@ -7,7 +9,7 @@
 # "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#	http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
@@ -17,11 +19,31 @@
 # under the License.
 #
 
-from distutils.core import setup
-
-setup(name='fb303',
-      version='1.0',
-      packages=['fb303', 'fb303_scripts'],
-      )
-
+import sys
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils.core import setup, Extension, Command
+        
+setup(name = 'thrift',
+    version = '0.9.0-dev',
+    description = 'Python bindings for the Apache Thrift FB303',
+    author = ['Thrift Developers'],
+    author_email = ['dev@thrift.apache.org'],
+    url = 'http://thrift.apache.org',
+    license = 'Apache License 2.0',
+    packages = [
+        'fb303',
+        'fb303_scripts',
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Networking'
+    ],
+)
 
