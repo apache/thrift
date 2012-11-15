3806a743c8237255f292d889902a4fed0d094842
Roger Meier <roger@apache.org>
2012-11-15 21:46:01 +0000

    THRIFT-1747 Generated Python code incorrect when declaring constants of a struct type declared in an included thrift file Patch: Justin Judd
    git-svn-id: https://svn.apache.org/repos/asf/thrift/trunk@1410038 13f79535-47bb-0310-9956-ffa450edef68

--- compiler/cpp/src/generate/t_py_generator.cc
+++ compiler/cpp/src/generate/t_py_generator.cc
@@ -518,7 +518,7 @@ string t_py_generator::render_const_value(t_type* type, t_const_value* value) {
   } else if (type->is_enum()) {
     indent(out) << value->get_integer();
   } else if (type->is_struct() || type->is_xception()) {
-    out << type->get_name() << "(**{" << endl;
+    out << type_name(type) << "(**{" << endl;
     indent_up();
     const vector<t_field*>& fields = ((t_struct*)type)->get_members();
     vector<t_field*>::const_iterator f_iter;
