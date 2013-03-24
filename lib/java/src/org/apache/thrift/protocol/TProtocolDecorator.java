cac2c5761e2ebe8b14a3c03dd2ee756c1e14441b
Jens Geyer <jensg@apache.org>
2013-03-25 01:22:09 +0200

    THRIFT-563 Support for Multiplexing Services on any Transport, Protocol and Server
    Patch: Rob Slifka
    Fixes broken build due to previous patch

--- lib/java/src/org/apache/thrift/protocol/TProtocolDecorator.java
+++ lib/java/src/org/apache/thrift/protocol/TProtocolDecorator.java
@@ -1,6 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.thrift.protocol;
 
 import org.apache.thrift.TException;
+
+import java.nio.ByteBuffer;
 
 /**
  * <code>TProtocolDecorator</code> forwards all requests to an enclosed
@@ -106,8 +127,8 @@ public abstract class TProtocolDecorator extends TProtocol {
         concreteProtocol.writeString(s);
     }
 
-    public void writeBinary(byte[] bytes) throws TException {
-        concreteProtocol.writeBinary(bytes);
+    public void writeBinary(ByteBuffer buf) throws TException {
+        concreteProtocol.writeBinary(buf);
     }
 
     public TMessage readMessageBegin() throws TException {
@@ -186,7 +207,7 @@ public abstract class TProtocolDecorator extends TProtocol {
         return concreteProtocol.readString();
     }
 
-    public byte[] readBinary() throws TException {
+    public ByteBuffer readBinary() throws TException {
         return concreteProtocol.readBinary();
     }
 }
