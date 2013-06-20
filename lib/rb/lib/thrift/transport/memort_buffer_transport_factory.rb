221840c951098f6661418b4d892f6a4d8b1134d6
Akos Vandra <avandra@whitepages.com>
2013-06-20 11:39:21 -0700

    Added factories for the protocols

---
+++ lib/rb/lib/thrift/transport/memort_buffer_transport_factory.rb
@@ -0,0 +1,19 @@
+
+require 'thrift'
+
+class MemoryBufferTransportFactory
+	def createTransport
+		MemoryBufferTransport.new
+	end
+end
+
+class FastMemoryBufferTransportFactory
+	def createTransport
+		if defined? FastMemoryBufferTransport then
+			FastMemoryBufferTransport.new
+		else
+			puts "Falling back to MemoryBufferTransport from FastMemoryBufferTransport";
+			MemoryBufferTransport.new
+		end
+	end
+end
