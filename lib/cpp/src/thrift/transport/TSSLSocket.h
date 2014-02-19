01386c95a8f18d55cefc0ad0f33a1154e095f51a
Roger Meier <roger@apache.org>
2014-02-19 23:07:25 +0100

    THRIFT-2258 cpp: Add TLS v1.1/1.2 support to TSSLSocket.cpp Patch: Chris Stylianou

--- lib/cpp/src/thrift/transport/TSSLSocket.h
+++ lib/cpp/src/thrift/transport/TSSLSocket.h
@@ -31,6 +31,15 @@ namespace apache { namespace thrift { namespace transport {
 class AccessManager;
 class SSLContext;
 
+enum SSLProtocol {
+  SSLTLS  = 0,  // Supports SSLv3 and TLSv1.
+  SSLv2   = 1,  // Supports SSLv3 only. => HORRIBLY INSECURE!
+  SSLv3   = 2,  // Supports SSLv3 only.
+  TLSv1_0 = 3,  // Supports TLSv1_0 only.
+  TLSv1_1 = 4,  // Supports TLSv1_1 only.
+  TLSv1_2 = 5   // Supports TLSv1_2 only.
+};
+
 /**
  * OpenSSL implementation for SSL socket interface.
  */
@@ -108,8 +117,10 @@ class TSSLSocketFactory {
  public:
   /**
    * Constructor/Destructor
+   *
+   * @param protocol The SSL/TLS protocol to use.
    */
-  TSSLSocketFactory();
+  TSSLSocketFactory(const SSLProtocol& protocol = SSLProtocol::SSLTLS);
   virtual ~TSSLSocketFactory();
   /**
    * Create an instance of TSSLSocket with a fresh new socket.
@@ -234,7 +245,7 @@ class TSSLException: public TTransportException {
  */
 class SSLContext {
  public:
-  SSLContext();
+  SSLContext(const SSLProtocol& protocol = SSLProtocol::SSLTLS);
   virtual ~SSLContext();
   SSL* createSSL();
   SSL_CTX* get() { return ctx_; }
