5c4e970597e60d4cec8cb048c42954999026c272
jfarrell <jfarrell@apache.org>
2014-04-04 11:41:15 -0400

    THRIFT-2204:SSL client for the cocoa client Client: cocoa Patch: Olivier Van Acker
    Adds a SSL transport to the cocoa library

--- lib/cocoa/src/transport/TSSLSocketClient.h
+++ lib/cocoa/src/transport/TSSLSocketClient.h
@@ -18,23 +18,23 @@
  */
 
 #import <Foundation/Foundation.h>
-#import "TTransport.h"
+#import "TNSStreamTransport.h"
 
-@interface TNSStreamTransport : NSObject <TTransport> {
-
+@interface TSSLSocketClient : TNSStreamTransport
+#if TARGET_OS_IPHONE || (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6)
+<NSStreamDelegate>
+#endif
+{
+    NSInputStream *inputStream;
+    NSOutputStream *outputStream;
+@private
+    NSString *sslHostname;
+    int sd;
 }
 
-@property (nonatomic, strong) NSInputStream * mInput;
-@property (nonatomic, strong) NSOutputStream * mOutput;
+- (id) initWithHostname: (NSString *) hostname
+                   port: (int) port;
 
-- (id) initWithInputStream: (NSInputStream *) input
-              outputStream: (NSOutputStream *) output;
-
-- (id) initWithInputStream: (NSInputStream *) input;
-
-- (id) initWithOutputStream: (NSOutputStream *) output;
+- (BOOL) isOpen;
 
 @end
-
-
-
