3aa5679e35888e786c40c65339e3f1ebc3010158
hpgood <35557088@qq.com>
2014-06-06 11:42:47 +0800

    Update json_protocol.go
    at ReadMessageEnd,don't need ParsePostValue.

--- lib/go/thrift/json_protocol.go
+++ lib/go/thrift/json_protocol.go
@@ -236,7 +236,7 @@ func (p *TJSONProtocol) ReadMessageBegin() (name string, typeId TMessageType, se
 }
 
 func (p *TJSONProtocol) ReadMessageEnd() error {
-	err := p.ParseListEnd()
+	err := p._ParseListEnd(false)
 	return err
 }
 
