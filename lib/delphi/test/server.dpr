f868cc9656cad31b04aa9b7f15863e9705c9a819
Jens Geyer <jensg@apache.org>
2014-09-07 01:45:56 +0200

    THRIFT-2694 Minor glitches in Delphi cross-test code Client: Delphi Patch: Jens Geyer

--- lib/delphi/test/server.dpr
+++ lib/delphi/test/server.dpr
@@ -34,6 +34,7 @@ uses
   Thrift.Collections in '..\src\Thrift.Collections.pas',
   Thrift.Server in '..\src\Thrift.Server.pas',
   Thrift.Console in '..\src\Thrift.Console.pas',
+  Thrift.TypeRegistry in '..\src\Thrift.TypeRegistry.pas',
   Thrift.Utils in '..\src\Thrift.Utils.pas',
   Thrift.Stream in '..\src\Thrift.Stream.pas';
 
