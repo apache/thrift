20eb790ca8e6fb1254f506b73980ac59d805306b
Jens Geyer <jensg@apache.org>
2014-10-01 03:00:19 +0200

    THRIFT-2665 Debug statements are unnecessary and should be removed Client: Python Patch: Dustin Spicuzza
    This closes #189

--- lib/py/src/TTornado.py
+++ lib/py/src/TTornado.py
@@ -137,9 +137,7 @@ class TTornadoStreamTransport(TTransportBase):
                 if len(frame_header) == 0:
                     raise iostream.StreamClosedError('Read zero bytes from stream')
                 frame_length, = struct.unpack('!i', frame_header)
-                logger.debug('received frame header, frame length = %d', frame_length)
                 frame = yield self.stream.read_bytes(frame_length)
-                logger.debug('received frame payload: %r', frame)
                 raise gen.Return(frame)
 
     def write(self, buf):
