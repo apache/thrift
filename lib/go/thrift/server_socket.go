ac72ce1e514d837e1dc25e69f633723767e22819
Jens Geyer <jensg@apache.org>
2014-11-11 00:11:16 +0100

    THRIFT-2812 Go server adding redundant buffering layer Client: Go Patch: Craig Peterson
    This closes #259
    
    It has been pointed out to me that #249 was wrong. It was not needed to allow server sockets to use buffering. The correct way is to pass in a TBufferedTransportFactory to the server. This will create buffered sockets as the processor starts up.
    
    This change creates extra buffering, and is actually a real pain to deactivate. Sorry.

--- lib/go/thrift/server_socket.go
+++ lib/go/thrift/server_socket.go
@@ -33,10 +33,6 @@ type TServerSocket struct {
 	// Protects the interrupted value to make it thread safe.
 	mu          sync.RWMutex
 	interrupted bool
-
-	//Size of buffer to use for socket. Defaults to 1024.
-	//Set to 0 to disable bufferring server transport altogether.
-	BufferSize int
 }
 
 func NewTServerSocket(listenAddr string) (*TServerSocket, error) {
@@ -48,7 +44,7 @@ func NewTServerSocketTimeout(listenAddr string, clientTimeout time.Duration) (*T
 	if err != nil {
 		return nil, err
 	}
-	return &TServerSocket{addr: addr, clientTimeout: clientTimeout, BufferSize: 1024}, nil
+	return &TServerSocket{addr: addr, clientTimeout: clientTimeout}, nil
 }
 
 func (p *TServerSocket) Listen() error {
@@ -78,12 +74,7 @@ func (p *TServerSocket) Accept() (TTransport, error) {
 	if err != nil {
 		return nil, NewTTransportExceptionFromError(err)
 	}
-	var trans TTransport
-	trans = NewTSocketFromConnTimeout(conn, p.clientTimeout)
-	if p.BufferSize != 0 {
-		trans = NewTBufferedTransport(trans, p.BufferSize)
-	}
-	return trans, nil
+	return NewTSocketFromConnTimeout(conn, p.clientTimeout), nil
 }
 
 // Checks whether the socket is listening.
