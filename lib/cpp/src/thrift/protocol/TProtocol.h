721a3622215f6c09d56fa71ce9bde845ae367a5e
Dave Watson <davejwatson@fb.com>
2015-01-16 11:22:01 -0800

    THRIFT-2423 Facebook's THeader protocol and transport for cpp Client: C++ Library, Compiler Patch: Dave Watson rebased by Nobuaki Sukegawa
    This closes #357 and closes #677

--- lib/cpp/src/thrift/protocol/TProtocol.h
+++ lib/cpp/src/thrift/protocol/TProtocol.h
@@ -595,6 +595,11 @@ public:
   virtual ~TProtocolFactory();
 
   virtual boost::shared_ptr<TProtocol> getProtocol(boost::shared_ptr<TTransport> trans) = 0;
+  virtual boost::shared_ptr<TProtocol> getProtocol(boost::shared_ptr<TTransport> inTrans,
+						   boost::shared_ptr<TTransport> outTrans) {
+    (void)outTrans;
+    return getProtocol(inTrans);
+  }
 };
 
 /**
