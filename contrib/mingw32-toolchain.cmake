ded6401da41fa20ca4d1614957ea09f72a300c02
Randy Abernethy <ra@apache.org>
2015-02-15 11:28:40 -0800

    THRIFT-2999 remove CR/LF post .getattributes text = auto Client: all Patch: Randy Abernethy

--- contrib/mingw32-toolchain.cmake
+++ contrib/mingw32-toolchain.cmake
@@ -12,7 +12,7 @@ SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
 SET(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc)
 
 # adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search 
+# search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
