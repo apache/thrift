7d96152207e2b71a173432f80a056fa45180b737
Mathijs van de Nes <git@mathijs.vd-nes.nl>
2015-03-30 08:44:14 +0200

    Add build script for rs tutorial

---
+++ tutorial/rs/build.rs
@@ -0,0 +1,22 @@
+fn main() {
+    build_thrift("../shared.thrift");
+    build_thrift("../tutorial.thrift");
+}
+
+fn build_thrift(thriftfile: &'static str) {
+    let thrift = std::env::var("THRIFT").unwrap_or("../../compiler/cpp/thrift".to_string());
+    let target = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut cmd = std::process::Command::new(thrift);
+    cmd.current_dir(target)
+        .args(&["--gen", "rs"])
+        .args(&["--out", "src"])
+        .arg(thriftfile);
+    let status = cmd.status().ok().expect("Could not execute thrift");
+    if !status.success() {
+        let code = match status.code() {
+            Some(c) => format!("{}", c),
+            None => "?".to_string(),
+        };
+        panic!("Thrift exited with code: {}", code);
+    }
+}
