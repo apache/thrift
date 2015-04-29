f2898ff23b529a584c104989c320b450248da292
Intropy <jbolla@gmail.com>
2015-04-28 17:25:16 -0700

    Add thread class to the run() failure log message

--- lib/java/src/org/apache/thrift/server/TThreadedSelectorServer.java
+++ lib/java/src/org/apache/thrift/server/TThreadedSelectorServer.java
@@ -375,7 +375,7 @@ public class TThreadedSelectorServer extends AbstractNonblockingServer {
           select();
         }
       } catch (Throwable t) {
-        LOGGER.error("run() exiting due to uncaught error", t);
+        LOGGER.error("run() on AcceptThread exiting due to uncaught error", t);
       } finally {
         try {
           acceptSelector.close();
@@ -546,7 +546,7 @@ public class TThreadedSelectorServer extends AbstractNonblockingServer {
           cleanupSelectionKey(selectionKey);
         }
       } catch (Throwable t) {
-        LOGGER.error("run() exiting due to uncaught error", t);
+        LOGGER.error("run() on SelectorThread exiting due to uncaught error", t);
       } finally {
         try {
           selector.close();
