83291d362e47201b3aca6d491596015af56967e7
Jens Geyer <jensg@apache.org>
2015-04-30 20:29:20 +0200

    THRIFT-3120 Minor spelling errors and an outdated URL Client: C++ Patch: Calvin Sun <675313675@qq.com>
    This closes #471

--- lib/cpp/src/thrift/protocol/TProtocol.h
+++ lib/cpp/src/thrift/protocol/TProtocol.h
@@ -39,8 +39,7 @@
 // The most obvious implementation is to just cast a pointer,
 // but that doesn't work.
 // For a pretty in-depth explanation of the problem, see
-// http://www.cellperformance.com/mike_acton/2006/06/ (...)
-// understanding_strict_aliasing.html
+// http://cellperformance.beyond3d.com/articles/2006/06/understanding-strict-aliasing.html
 template <typename To, typename From>
 static inline To bitwise_cast(From from) {
   BOOST_STATIC_ASSERT(sizeof(From) == sizeof(To));
