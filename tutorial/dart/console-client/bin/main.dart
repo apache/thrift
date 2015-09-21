eaa54dedb5aa92117515b8cf5a9f4cf0ae1d6a00
Mark Erickson <mark.erickson@workiva.com>
2015-09-21 09:34:06 -0500

    Create an Apache Thrift language binding for Dart (dartlang.org). https://issues.apache.org/jira/browse/THRIFT-3299
    Add license files and fomatted.

--- tutorial/dart/console-client/bin/main.dart
+++ tutorial/dart/console-client/bin/main.dart
@@ -1,3 +1,20 @@
+/// Licensed to the Apache Software Foundation (ASF) under one
+/// or more contributor license agreements. See the NOTICE file
+/// distributed with this work for additional information
+/// regarding copyright ownership. The ASF licenses this file
+/// to you under the Apache License, Version 2.0 (the
+/// "License"); you may not use this file except in compliance
+/// with the License. You may obtain a copy of the License at
+///
+/// http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing,
+/// software distributed under the License is distributed on an
+/// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+/// KIND, either express or implied. See the License for the
+/// specific language governing permissions and limitations
+/// under the License.
+
 import 'dart:async';
 import 'dart:io';
 
@@ -33,7 +50,8 @@ main(List<String> args) {
 
 Future _initConnection(int port) async {
   var socket = await Socket.connect('127.0.0.1', port);
-  _transport = new TClientSocketTransport(new TTcpSocket(socket), new TBinaryProtocolFactory());
+  _transport = new TClientSocketTransport(
+      new TTcpSocket(socket), new TBinaryProtocolFactory());
   TProtocol protocol = new TBinaryProtocol(_transport);
   await _transport.open();
 
@@ -67,10 +85,10 @@ Future _run() async {
         }
 
         var work = new Work()
-            ..num1 = int.parse(args[0])
-            ..op = op
-            ..num2 = int.parse(args[2])
-            ..comment = args.length > 3 ? args[3] : '';
+          ..num1 = int.parse(args[0])
+          ..op = op
+          ..num2 = int.parse(args[2])
+          ..comment = args.length > 3 ? args[3] : '';
 
         await _calc(work);
         break;
@@ -84,7 +102,6 @@ Future _run() async {
         _help();
         break;
     }
-
   }
 }
 
