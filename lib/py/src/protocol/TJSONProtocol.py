15c400488550d565e5436f1e77ad3fdddfd4ef66
Jens Geyer <jensg@apache.org>
2015-09-29 21:33:23 +0200

    THRIFT-3323 Python library does not handle escaped forward slash ("/") in JSON Client: Python Patch: Grepsr <info@grepsr.com>

--- lib/py/src/protocol/TJSONProtocol.py
+++ lib/py/src/protocol/TJSONProtocol.py
@@ -41,8 +41,8 @@ BACKSLASH = '\\'
 ZERO = '0'
 
 ESCSEQ = '\\u00'
-ESCAPE_CHAR = '"\\bfnrt'
-ESCAPE_CHAR_VALS = ['"', '\\', '\b', '\f', '\n', '\r', '\t']
+ESCAPE_CHAR = '"\\bfnrt/'
+ESCAPE_CHAR_VALS = ['"', '\\', '\b', '\f', '\n', '\r', '\t', '/']
 NUMERIC_CHAR = '+-.0123456789Ee'
 
 CTYPES = {TType.BOOL:       'tf',
