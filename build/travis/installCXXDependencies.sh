2e37f10578639d26100e5f988ae5747d2abfb4e6
James E. King, III <jim.king@simplivity.com>
2015-11-04 16:50:43 -0500

    fix travis dependencies

--- build/travis/installCXXDependencies.sh
+++ build/travis/installCXXDependencies.sh
@@ -31,5 +31,9 @@ sudo add-apt-repository ppa:smspillaz/cmake-2.8.12 -y
 sudo apt-add-repository ppa:ubuntu-sdk-team/ppa -y
 
 sudo apt-get update -qq
+
+# remove cmake from ubuntu
+sudo apt-get remove cmake cmake-data
+
 sudo apt-get install libpango1.0-0 libqt4-dev qtbase5-dev qtbase5-dev-tools qt5-default libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make cmake git debhelper bc nsis ninja-build
 dpkg -S /usr/include/boost/version.hpp
