8551f3f53dee2a879de0aa511471f0190b1fe71c
Nobuaki Sukegawa <nsuke@apache.org>
2015-11-15 16:33:14 +0900

    Revert part of 7f404fdef23dbbe5204d35bcb9261ff50939f31f
    unichr does not work for code point > 0x10000 on ucs2 build

--- lib/py/src/protocol/TJSONProtocol.py
+++ lib/py/src/protocol/TJSONProtocol.py
@@ -251,14 +251,18 @@ class TJSONProtocolBase(TProtocolBase):
 
   def _toChar(self, high, low=None):
     if not low:
-      codepoint = high
+      if sys.version_info[0] == 2:
+        return ("\\u%04x" % high).decode('unicode-escape').encode('utf-8')
+      else:
+        return chr(high)
     else:
       codepoint = (1 << 16) + ((high & 0x3ff) << 10)
       codepoint += low & 0x3ff
-    if sys.version_info[0] == 2:
-      return unichr(codepoint).encode('utf-8')
-    else:
-      return chr(codepoint)
+      if sys.version_info[0] == 2:
+        s = "\\U%08x" % codepoint
+        return s.decode('unicode-escape').encode('utf-8')
+      else:
+        return chr(codepoint)
 
   def readJSONString(self, skipContext):
     highSurrogate = None
