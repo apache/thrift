656d7ad24b0984dbc959b7130c97e1d128a400bb
Sebastian Zenker <sebastian.zenker@gmx.de>
2015-12-18 16:21:12 +0100

    fixed: TEvhttpClientChannel - when sending a second request (via sendAndRecvMessage()) before the first cob of the first request has signaled that a response is available, the cob of the first request gets overwritten by the second request. Consequently, the cob of the first request never gets called and cob of the second gets called twice.

--- lib/cpp/src/thrift/async/TEvhttpClientChannel.h
+++ lib/cpp/src/thrift/async/TEvhttpClientChannel.h
@@ -20,7 +20,9 @@
 #ifndef _THRIFT_TEVHTTP_CLIENT_CHANNEL_H_
 #define _THRIFT_TEVHTTP_CLIENT_CHANNEL_H_ 1
 
+#include <queue>
 #include <string>
+#include <utility>
 #include <boost/shared_ptr.hpp>
 #include <thrift/async/TAsyncChannel.h>
 
@@ -72,8 +74,9 @@ private:
 
   std::string host_;
   std::string path_;
-  VoidCallback cob_;
-  apache::thrift::transport::TMemoryBuffer* recvBuf_;
+  using Completion = std::pair<VoidCallback, apache::thrift::transport::TMemoryBuffer*>;
+  using CompletionQueue = std::queue<Completion>;
+  CompletionQueue completionQueue_;
   struct evhttp_connection* conn_;
 };
 }
