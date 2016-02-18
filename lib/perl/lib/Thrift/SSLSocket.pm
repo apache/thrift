36d448636671b7371d362a1e06a4b04f7a2749d8
Nobuaki Sukegawa <nsuke@apache.org>
2016-02-19 00:38:10 +0900

    THRIFT-3643 Perl SSL server crushes if a client disconnect without handshake

--- lib/perl/lib/Thrift/SSLSocket.pm
+++ lib/perl/lib/Thrift/SSLSocket.pm
@@ -54,7 +54,9 @@ sub __close
 {
     my $self = shift;
     my $sock = ($self->{handle}->handles())[0];
-    $sock->close(SSL_no_shutdown => 1);
+    if ($sock) {
+      $sock->close(SSL_no_shutdown => 1);
+    }
 }
 
 sub __recv
@@ -63,8 +65,10 @@ sub __recv
 	my $sock = shift;
 	my $len = shift;
 	my $buf = undef;
+  if ($sock) {
     sysread($sock, $buf, $len);
-    return $buf;
+  }
+  return $buf;
 }
 
 sub __send
@@ -79,7 +83,7 @@ sub __wait
 {
     my $self = shift;
     my $sock = ($self->{handle}->handles())[0];
-    if ($sock->pending() eq 0) {
+    if ($sock and $sock->pending() eq 0) {
         return $self->SUPER::__wait();
     }
     return $sock;
