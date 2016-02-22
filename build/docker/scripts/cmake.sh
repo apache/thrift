d0164799cdc220d111075d6501a1fe2071749b73
Nobuaki Sukegawa <nsuke@apache.org>
2016-02-22 21:29:13 +0900

    THRIFT-3659 Check configure result of CMake on CI
    This closes #886

--- build/docker/scripts/cmake.sh
+++ build/docker/scripts/cmake.sh
@@ -3,6 +3,12 @@ set -ev
 
 mkdir -p cmake_build && cd cmake_build
 cmake $* ..
+for LIB in $BUILD_LIBS; do
+  if ! grep "^BUILD_${LIB}:BOOL=ON$" CMakeCache.txt ; then
+    echo "failed to configure $LIB"
+    exit 1
+  fi
+done
 make -j3
 cpack
 ctest -VV -E "(concurrency_test|processor_test)"
