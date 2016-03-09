72f0814b7704271fe900db16eb63b169db5d007d
zhujun <zhujun@optaim.com>
2016-03-09 18:23:43 +0800

    rm dumplicate err info

--- lib/go/thrift/simple_server.go
+++ lib/go/thrift/simple_server.go
@@ -176,7 +176,6 @@ func (p *TSimpleServer) processRequests(client TTransport) error {
 		if err, ok := err.(TTransportException); ok && err.TypeId() == END_OF_FILE {
 			return nil
 		} else if err != nil {
-			log.Printf("error processing request: %s", err)
 			return err
 		}
 		if !ok {
