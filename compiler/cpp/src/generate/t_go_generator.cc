f426833156a3bc16f425d349c365961783c5afc4
creker <sam901@yandex.ru>
2016-04-04 17:11:33 +0300

    Fix Go code generation when namespace is not specified

--- compiler/cpp/src/generate/t_go_generator.cc
+++ compiler/cpp/src/generate/t_go_generator.cc
@@ -777,7 +777,7 @@ string t_go_generator::render_included_programs(string& unused_protection) {
 
   string local_namespace = program_->get_namespace("go");
   for (size_t i = 0; i < includes.size(); ++i) {
-    if (local_namespace == includes[i]->get_namespace("go")) {
+    if (!local_namespace.empty() && local_namespace == includes[i]->get_namespace("go")) {
       continue;
     }
 
@@ -809,7 +809,7 @@ string t_go_generator::render_includes(bool consts) {
 
   string local_namespace = program_->get_namespace("go");
   for (size_t i = 0; i < includes.size(); ++i) {
-    if (local_namespace == includes[i]->get_namespace("go")) {
+    if (!local_namespace.empty() && local_namespace == includes[i]->get_namespace("go")) {
       continue;
     }
 
@@ -3452,14 +3452,16 @@ string t_go_generator::type_name(t_type* ttype) {
 string t_go_generator::module_name(t_type* ttype) {
   t_program* program = ttype->get_program();
 
-  if (program != NULL && program != program_ && program->get_namespace("go") != program_->get_namespace("go")) {
-    string module(get_real_go_module(program));
-    // for namespaced includes, only keep part after dot.
-    size_t dot = module.rfind('.');
-    if (dot != string::npos) {
-      module = module.substr(dot + 1);
+  if (program != NULL && program != program_) {
+    if (program->get_namespace("go").empty() || program_->get_namespace("go").empty() || program->get_namespace("go") != program_->get_namespace("go")) {
+      string module(get_real_go_module(program));
+      // for namespaced includes, only keep part after dot.
+      size_t dot = module.rfind('.');
+      if (dot != string::npos) {
+        module = module.substr(dot + 1);
+      }
+      return module;
     }
-    return module;
   }
 
   return "";
