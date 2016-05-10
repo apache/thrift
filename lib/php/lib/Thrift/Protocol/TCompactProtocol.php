07a334de440c82a4f413dda46cbd39fb221e7ba8
lnn1123 <lnn1123killer@gmail.com>
2016-05-10 18:44:20 +0800

    THRIFT-3827 Fix CompactProtocol readI64 function
    This closes #1008 and closes #1052

--- lib/php/lib/Thrift/Protocol/TCompactProtocol.php
+++ lib/php/lib/Thrift/Protocol/TCompactProtocol.php
@@ -615,7 +615,7 @@ class TCompactProtocol extends TProtocol
       if ($shift >= 32) {
         $hi |= (($byte & 0x7f) << ($shift - 32));
       } elseif ($shift > 25) {
-        $hi |= (($byte & 0x7f) >> ($shift - 25));
+        $hi |= (($byte & 0x7f) >> ($shift - 24));
       }
       if (($byte >> 7) === 0) {
         break;
@@ -635,7 +635,7 @@ class TCompactProtocol extends TProtocol
 
     // Now put $hi and $lo back together
     if (true) {
-      $isNeg = $hi  < 0;
+      $isNeg = $hi  < 0 || $hi & 0x80000000;
 
       // Check for a negative
       if ($isNeg) {
