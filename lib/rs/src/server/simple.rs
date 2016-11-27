98dc1ee79d81a00df92e06ba63491f340a0f5981
Allen George <allen.george@gmail.com>
2016-11-27 12:22:53 -0500

    Incomplete implementation of simple server

--- lib/rs/src/server/simple.rs
+++ lib/rs/src/server/simple.rs
@@ -15,59 +15,58 @@
 // specific language governing permissions and limitations
 // under the License.
 
-/*
 use std::cell::RefCell;
-use std::net::{Shutdown, TcpListener, TcpStream};
+use std::marker::PhantomData;
+use std::net::{TcpListener, TcpStream};
 use std::rc::Rc;
 
 use ::{ApplicationError, ApplicationErrorKind};
-use ::protocol::TProtocolFactory;
-use ::transport::{TTransport, TTcpTransport, TTransportFactory};
+use ::protocol::{TProtocol, TProtocolFactory};
+use ::transport::{RcTTransport, TTcpTransport, TTransport, TTransportFactory};
 use super::TProcessor;
-*/
 
-pub struct TSimpleServer {}
-
-/*
-pub struct TSimpleServer<TFI, TRI, PFI, TFO, TRO, PFO, PRC>
-    where
-        TFI: TTransportFactory<TRI>, TRI: TTransport,
-        PFI: TProtocolFactory,
-        TFO: TTransportFactory<TRO>, TRO: TTransport,
-        PFO: TProtocolFactory,
-        PRC: TProcessor {
-    i_tran_factory: TFI,
-    i_prot_factory: PFI,
-    o_tran_factory: TFO,
-    o_prot_factory: PFO,
-    processor: PRC,
+pub struct TSimpleServer<TI, TFI, PI, PFI, TO, TFO, PO, PFO, PR>
+    where TI: TTransport + 'static, TFI: TTransportFactory<TI>, PI: TProtocol + 'static, PFI: TProtocolFactory<PI>,
+          TO: TTransport + 'static, TFO: TTransportFactory<TO>, PO: TProtocol + 'static, PFO: TProtocolFactory<PO>,
+          PR: TProcessor {
+    i_trans_type: PhantomData<TI>,
+    i_trans_factory: TFI,
+    i_proto_type: PhantomData<PI>,
+    i_proto_factory: PFI,
+    o_trans_type: PhantomData<TO>,
+    o_trans_factory: TFO,
+    o_proto_type: PhantomData<PO>,
+    o_proto_factory: PFO,
+    processor: PR,
 }
 
-impl <TFI, TRI, PFI, TFO, TRO, PFO, PRC> TSimpleServer<TFI, TRI, PFI, TFO, TRO, PFO, PRC>
-    where
-        TFI: TTransportFactory<TRI>, TRI: TTransport,
-        PFI: TProtocolFactory,
-        TFO: TTransportFactory<TFO>, TFO: TTransport,
-        PFO: TProtocolFactory,
-        PRC: TProcessor {
+impl <TI, TFI, PI, PFI, TO, TFO, PO, PFO, PR> TSimpleServer<TI, TFI, PI, PFI, TO, TFO, PO, PFO, PR>
+
+    where TI: TTransport + 'static, TFI: TTransportFactory<TI>, PI: TProtocol + 'static, PFI: TProtocolFactory<PI>,
+          TO: TTransport + 'static, TFO: TTransportFactory<TO>, PO: TProtocol + 'static, PFO: TProtocolFactory<PO>,
+          PR: TProcessor {
 
     pub fn new(
         input_transport_factory: TFI,
         input_protocol_factory: PFI,
         output_transport_factory: TFO,
         output_protocol_factory: PFO,
-        processor: PRC
-    ) -> TSimpleServer<TFI, TRI, PFI, TFO, TRO, PFO, PRC> {
+        processor: PR
+    ) -> TSimpleServer<TI, TFI, PI, PFI, TO, TFO, PO, PFO, PR> {
         TSimpleServer {
-            i_tran_factory: input_transport_factory,
-            i_prot_factory: input_protocol_factory,
-            o_tran_factory: output_transport_factory,
-            o_prot_factory: output_protocol_factory,
+            i_trans_type: PhantomData,
+            i_trans_factory: input_transport_factory,
+            i_proto_type: PhantomData,
+            i_proto_factory: input_protocol_factory,
+            o_trans_type: PhantomData,
+            o_trans_factory: output_transport_factory,
+            o_proto_type: PhantomData,
+            o_proto_factory: output_protocol_factory,
             processor: processor
         }
     }
 
-    pub fn listener(&mut self, listening_address: &str) -> ::Result<()> {
+    pub fn listen(&mut self, listening_address: &str) -> ::Result<()> {
         let listener = try!(TcpListener::bind(listening_address));
         for stream in listener.incoming() {
             match stream {
@@ -92,34 +91,35 @@ impl <TFI, TRI, PFI, TFO, TRO, PFO, PRC> TSimpleServer<TFI, TRI, PFI, TFO, TRO, 
 
     fn handle_incoming_connection(&mut self, stream: TcpStream) {
         // create the base transport (both input/output share this underlying stream)
-        let stream_transport = TTcpTransport::using_stream(stream);
-        let boxed_stream_transport: Box<TTransport> = Box::new(stream_transport);
-        let ref_stream_transport = Rc::new(RefCell::new(boxed_stream_transport));
+        let stream = TTcpTransport::using_stream(stream);
+        let box_stream: Box<TTransport> = Box::new(stream);
+        let ref_stream = Rc::new(RefCell::new(box_stream));
 
         // input protocol and transport
-        let i_tran = Box::new(self.i_tran_factory.new(ref_stream_transport.clone()));
-        let ref_i_tran = Rc::new(RefCell::new(i_tran));
-        let i_prot = self.i_prot_factory.new(ref_i_tran);
+        let i_tran = self.i_trans_factory.new(ref_stream.clone());
+        let ref_i_tran: RcTTransport = Rc::new(RefCell::new(Box::new(i_tran)));
+        let i_prot = self.i_proto_factory.new(ref_i_tran);
 
         // output protocol and transport
-        let o_tran = self.o_tran_factory.new(ref_stream_transport.clone());
-        let ref_o_tran = Rc::new(RefCell::new(Box::new(o_tran)));
-        let o_prot = self.o_prot_factory.new(ref_o_tran);
+        let o_tran = self.o_trans_factory.new(ref_stream.clone());
+        let ref_o_tran: RcTTransport = Rc::new(RefCell::new(Box::new(o_tran)));
+        let o_prot = self.o_proto_factory.new(ref_o_tran);
 
         // process loop
-        /*
         loop {
-            let r = self.processor.process(&**i_prot.borrow_mut(), &**o_prot.borrow_mut());
+            let r = self.processor.process(&i_prot, &o_prot);
             match r {
                 Err(e) => {
                     println!("processor failed with error {:?}", e);
-                    stream.shutdown(Shutdown::Both);
+//                    if let Err(e) = ref_stream.borrow_mut().close() {
+//                        println!("error {:?} while closing stream", e);
+//                    }
                     break;
                 },
                 Ok(_) => {
                     ()
                 },
             }
-        }*/
+        }
     }
-}*/
+}
