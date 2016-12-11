3324fb098a5039e4a9271f37d7544ada8acb0e8b
Allen George <allen.george@gmail.com>
2016-12-11 08:38:28 -0500

    Fix spacing

--- test/ThriftTest.thrift
+++ test/ThriftTest.thrift
@@ -116,11 +116,11 @@ struct CrazyNesting {
 }
 
 union SomeUnion {
- 1: map<Numberz, UserId> map_thing,
- 2: string string_thing,
- 3: i32 i32_thing,
- 4: Xtruct3 xtruct_thing,
- 5: Insanity insanity_thing
+  1: map<Numberz, UserId> map_thing,
+  2: string string_thing,
+  3: i32 i32_thing,
+  4: Xtruct3 xtruct_thing,
+  5: Insanity insanity_thing
 }
 
 exception Xception {
