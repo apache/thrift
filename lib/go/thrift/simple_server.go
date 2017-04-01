d5536d1ea0300d5643600608492ae51b35538cbf
James E. King, III <jking@apache.org>
2017-04-01 11:18:45 -0400

    THRIFT-3730: go server errors are logged twice Client: go Patch: zhujun2006 <stone1342006 at gmail dot com>
    This closes #940

--- lib/go/thrift/simple_server.go
+++ lib/go/thrift/simple_server.go
@@ -186,7 +186,6 @@ func (p *TSimpleServer) processRequests(client TTransport) error {
 		if err, ok := err.(TTransportException); ok && err.TypeId() == END_OF_FILE {
 			return nil
 		} else if err != nil {
-			log.Printf("error processing request: %s", err)
 			return err
 		}
 		if err, ok := err.(TApplicationException); ok && err.TypeId() == UNKNOWN_METHOD {
