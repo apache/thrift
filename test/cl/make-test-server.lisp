bd1f95e64607198233f68ce855e5aeefadcf7ee0
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-06 13:58:23 +0200

    Groundwork for integrating the cross-test suite for CL

---
+++ test/cl/make-test-server.lisp
@@ -0,0 +1,28 @@
+(in-package :cl-user)
+
+(require "asdf")
+(load "externals/bundle.lisp")
+(asdf:load-system :net.didierverna.clon)
+
+(net.didierverna.clon:nickname-package)
+
+(clon:defsynopsis (:postfix "FILES...")
+  (text :contents "A very short program.")
+  (group (:header "Immediate exit options:")
+    (flag :short-name "h" :long-name "help"
+          :description "Print this help and exit.")
+    (flag :short-name "v" :long-name "version"
+          :description "Print version number and exit.")))
+
+(defun main ()
+  "Entry point for our standalone application."
+  (clon:make-context)
+  (when (clon:getopt :short-name "h")
+    (clon:help)
+    (clon:exit))
+  (clon:do-cmdline-options (option name value source)
+    (print (list option name value source)))
+  (terpri)
+  (clon:exit))
+
+(clon:dump "TestServer" main)
