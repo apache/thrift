a632995a1ada9cfce5c9cd1c58b20ce2bd74c0a8
Tomek Kurcz <tomsandbox@gmail.com>
2017-10-18 11:09:26 +0200

    CL cross: Add the framed CL server to cross-tests

--- test/cl/make-test-server.lisp
+++ test/cl/make-test-server.lisp
@@ -16,22 +16,22 @@
     (flag :short-name "h" :long-name "help"
           :description "Print this help and exit.")
     (stropt :long-name "port"
-	    :description "Number of the port to listen for connections on."
-	    :default-value "9090"
-	    :argument-name "ARG"
-	    :argument-type :optional)
+            :description "Number of the port to listen for connections on."
+            :default-value "9090"
+            :argument-name "ARG"
+            :argument-type :optional)
     (stropt :long-name "server-type"
-	    :description "The type of server, currently only \"simple\" is available."
-	    :default-value "simple"
-	    :argument-name "ARG")
+            :description "The type of server, currently only \"simple\" is available."
+            :default-value "simple"
+            :argument-name "ARG")
     (stropt :long-name "transport"
-	    :description "Transport: currently only \"buffered\""
-	    :default-value "buffered"
-	    :argument-name "ARG")
+            :description "Transport: transport to use, one of: \"buffered\", \"framed\""
+            :default-value "buffered"
+            :argument-name "ARG")
     (stropt :long-name "protocol"
-	    :description "Protocol: currently only \"binary\""
-	    :default-value "binary"
-	    :argument-name "ARG")))
+            :description "Protocol: currently only \"binary\""
+            :default-value "binary"
+            :argument-name "ARG")))
 
 (defun main ()
   "Entry point for our standalone application."
@@ -39,16 +39,22 @@
   (when (clon:getopt :short-name "h")
     (clon:help)
     (clon:exit))
-  (let ((port "9090"))
+  (let ((port "9090")
+        (framed nil))
     (clon:do-cmdline-options (option name value source)
       (print (list option name value source))
       (if (string= name "port")
-	  (setf port value)))
+          (setf port value))
+      (if (string= name "transport")
+          (cond ((string= value "buffered") (setf framed nil))
+                ((string= value "framed") (setf framed t))
+                (t (error "Unsupported transport.")))))
     (terpri)
     (thrift:serve (puri:parse-uri (concatenate 'string
-					       "thrift://127.0.0.1:"
-					       port))
-		  thrift.test:thrift-test))
+                                               "thrift://127.0.0.1:"
+                                               port))
+                  thrift.test:thrift-test
+                  framed))
   (clon:exit))
 
 (clon:dump "TestServer" main)
