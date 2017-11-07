34df5e72d40d57895d6e1cc2285e6cf834da812a
Daniel Kochmanski <daniel@turtleware.eu>
2017-11-07 21:15:51 +0100

    cl: ensure-externals: don't check for a directory
    We wrap quicklisp-quickstart:install in ignore-errors, because QL will signal
    error if we try to install it in the same directory it already has been
    installed. In case we don't install it, we need to load quicklisp/setup.lisp to
    be able to use it. It doesn't hurt to load it after installation.

--- lib/cl/ensure-externals.sh
+++ lib/cl/ensure-externals.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-if [ ! -d "externals/" ] ; then
-    curl -O https://beta.quicklisp.org/quicklisp.lisp
-    sbcl --load quicklisp.lisp \
-         --eval "(quicklisp-quickstart:install :path \"quicklisp/\")" \
-         --eval "(quicklisp:bundle-systems '(#:usocket #:closer-mop #:trivial-utf-8 #:ieee-floats #:trivial-gray-streams #:alexandria #:bordeaux-threads #:cl-ppcre #:fiasco #:net.didierverna.clon) :to \"externals/\")" \
-         --eval "(quit)" \
-         --no-userinit
-fi
+curl -O https://beta.quicklisp.org/quicklisp.lisp
+sbcl --load quicklisp.lisp \
+     --eval "(ignore-errors (quicklisp-quickstart:install :path \"quicklisp/\"))" \
+     --eval "(load \"quicklisp/setup.lisp\")" \
+     --eval "(quicklisp:bundle-systems '(#:usocket #:closer-mop #:trivial-utf-8 #:ieee-floats #:trivial-gray-streams #:alexandria #:bordeaux-threads #:cl-ppcre #:fiasco #:net.didierverna.clon) :to \"externals/\")" \
+     --eval "(quit)" \
+     --no-userinit
