f9e90050b18112d586724807fd9faf289bd6f8c0
Gonzalo Aguilar Delgado <gaguilar@level2crm.com>
2017-11-13 20:36:51 +0100

    Avoid strings larger than testString("thing") to pass

--- test/c_glib/src/test_client.c
+++ test/c_glib/src/test_client.c
@@ -412,7 +412,7 @@ main (int argc, char **argv)
                                                         "2nd",
                                                         &error)) {
           printf (" = \"%s\"\n", string);
-          if (strncmp (string, "testString(\"2nd\")", sizeof("testString(\"2nd\")")) != 0) {
+          if (strcmp (string, "testString(\"2nd\")") != 0) {
             ++fail_count;
           }
 
