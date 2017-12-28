a15060acd81d72fa364a7aab327aa8c41d02958f
Robert Lu <robberphex@gmail.com>
2017-12-28 15:29:39 +0800

    THRIFT-4423: migrate to psr-4 Client: php
    This closes #1445

--- lib/php/lib/Thrift/TMultiplexedProcessor.php
+++ lib/php/lib/Thrift/TMultiplexedProcessor.php
@@ -25,7 +25,6 @@ namespace Thrift;
 use Thrift\Exception\TException;
 use Thrift\Protocol\TProtocol;
 use Thrift\Protocol\TMultiplexedProtocol;
-use Thrift\Protocol\TProtocolDecorator;
 use Thrift\Type\TMessageType;
 
 /**
@@ -114,30 +113,5 @@ class TMultiplexedProcessor
         return $processor->process(
             new StoredMessageProtocol($input, $messageName, $mtype, $rseqid), $output
         );
-    }
-}
-
-/**
- *  Our goal was to work with any protocol. In order to do that, we needed
- *  to allow them to call readMessageBegin() and get the Message in exactly
- *  the standard format, without the service name prepended to the Message name.
- */
-class StoredMessageProtocol extends TProtocolDecorator
-{
-    private $fname_, $mtype_, $rseqid_;
-
-    public function __construct(TProtocol $protocol, $fname, $mtype, $rseqid)
-    {
-        parent::__construct($protocol);
-        $this->fname_  = $fname;
-        $this->mtype_  = $mtype;
-        $this->rseqid_ = $rseqid;
-    }
-
-    public function readMessageBegin(&$name, &$type, &$seqid)
-    {
-        $name  = $this->fname_;
-        $type  = $this->mtype_;
-        $seqid = $this->rseqid_;
     }
 }
