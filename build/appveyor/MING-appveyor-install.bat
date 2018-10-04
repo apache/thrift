53f0859639856d0d65278845ebab00c4c9fba8e9
James E. King III <jking@apache.org>
2018-10-04 13:56:30 -0400

    fix appveyor mingw64 build

--- build/appveyor/MING-appveyor-install.bat
+++ build/appveyor/MING-appveyor-install.bat
@@ -25,13 +25,9 @@ CALL cl_banner_install.bat                 || EXIT /B
 CALL cl_setenv.bat                         || EXIT /B
 CALL cl_showenv.bat                        || EXIT /B
 
-:: We're going to keep boost at a version cmake understands
-SET BOOSTVER=1.64.0-3
-SET BOOSTPKG=mingw-w64-%MINGWPLAT%-boost-%BOOSTVER%-any.pkg.tar.xz
-SET IGNORE=--ignore mingw-w64-x86_64-boost --ignore mingw-w64-i686-boost
-
 SET PACKAGES=^
   --needed -S bison flex make ^
+  mingw-w64-%MINGWPLAT%-boost ^
   mingw-w64-%MINGWPLAT%-cmake ^
   mingw-w64-%MINGWPLAT%-libevent ^
   mingw-w64-%MINGWPLAT%-openssl ^
@@ -47,9 +43,3 @@ SET PACKAGES=^
 %BASH% -lc "pacman --noconfirm -Syu %IGNORE%"                       || EXIT /B
 %BASH% -lc "pacman --noconfirm -Su %IGNORE%"                        || EXIT /B
 %BASH% -lc "pacman --noconfirm %PACKAGES%"                          || EXIT /B
-
-:: Install a slightly older boost (BOOSTVER) as cmake in mingw
-:: does not have built-in dependencies for boost 1.66.0 yet
-:: -- this cuts down on build warning output --
-
-%BASH% -lc "if [[ $(pacman --query | grep '%MINGWPLAT%-boost') ^!= *"%BOOSTVER%"* ]]; then wget http://repo.msys2.org/mingw/%MINGWPLAT%/%BOOSTPKG% && pacman --noconfirm --needed -U %BOOSTPKG% && rm %BOOSTPKG%; fi" || EXIT /B
