631e02cd0906f255041c3e2699a2ba57785af67f
cyy <cyyever@outlook.com>
2019-01-05 14:49:24 +0800

    remove boost smart ptr

--- lib/cpp/src/thrift/stdcxx.h
+++ lib/cpp/src/thrift/stdcxx.h
@@ -87,42 +87,19 @@
 //
 ///////////////////////////////////////////////////////////////////
 
-// We can use std for memory functions only if the compiler supports template aliasing
-// The macro BOOST_NO_CXX11_SMART_PTR is defined as 1 under Visual Studio 2010 and 2012
-// which do not support the feature, so we must continue to use C++98 and boost on them.
-// We cannot use __cplusplus to detect this either, since Microsoft advertises an older one.
-
-#if defined(BOOST_NO_CXX11_SMART_PTR) || (defined(_MSC_VER) && _MSC_VER < 1800) || defined(FORCE_BOOST_SMART_PTR)
-#include <boost/smart_ptr.hpp>
-#else
 #include <memory>
-#endif
 
 namespace apache { namespace thrift { namespace stdcxx {
 
-#if defined(BOOST_NO_CXX11_SMART_PTR) || (defined(_MSC_VER) && _MSC_VER < 1800) || defined(FORCE_BOOST_SMART_PTR)
+using ::std::const_pointer_cast;
+using ::std::dynamic_pointer_cast;
+using ::std::enable_shared_from_this;
+using ::std::make_shared;
+template <typename T> using scoped_ptr = std::unique_ptr<T>;		// compiler must support template aliasing
+using ::std::shared_ptr;
+using ::std::static_pointer_cast;
+using ::std::weak_ptr;
 
-  using ::boost::const_pointer_cast;
-  using ::boost::dynamic_pointer_cast;
-  using ::boost::enable_shared_from_this;
-  using ::boost::make_shared;
-  using ::boost::scoped_ptr;
-  using ::boost::shared_ptr;
-  using ::boost::static_pointer_cast;
-  using ::boost::weak_ptr;
-
-#else
-
-  using ::std::const_pointer_cast;
-  using ::std::dynamic_pointer_cast;
-  using ::std::enable_shared_from_this;
-  using ::std::make_shared;
-  template <typename T> using scoped_ptr = std::unique_ptr<T>;		// compiler must support template aliasing
-  using ::std::shared_ptr;
-  using ::std::static_pointer_cast;
-  using ::std::weak_ptr;
-
-#endif
 
 }}} // apache::thrift::stdcxx
 
