7c4fdd7e9551a781c6e6d8465b0275faa1fbd10a
Andrew Boyle <andrew.boyle@reddit.com>
2020-04-03 18:32:56 -0700

    THRIFT-5164: add middleware framework for Go servers Client: go
    This commit adds a simple middleware framework for Go servers.
    
    It provides:
    
     * A `ProcessorMiddleware` function interface used to define the actual middleware
     * `WrapProcessor`, the function that you use to wrap a `TProcessor` in a list of middleware
     * A helper `WrappedTProcessorFunction` struct to help with developing middleware
    
    This is may be breaking change for any custom implementations of the `TProcessor`
    interface, but should not effect the code generated by compiling Thrift files.  It
    adds two functions to the interface that are a part of the generated `TProcessor`
    code, but were not defined in the interface explicitly.

--- lib/go/thrift/common_test.go
+++ lib/go/thrift/common_test.go
@@ -19,7 +19,10 @@
 
 package thrift
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type mockProcessor struct {
 	ProcessFunc func(in, out TProtocol) (bool, TException)
@@ -28,3 +31,76 @@ type mockProcessor struct {
 func (m *mockProcessor) Process(ctx context.Context, in, out TProtocol) (bool, TException) {
 	return m.ProcessFunc(in, out)
 }
+
+func (m *mockProcessor) ProcessorMap() map[string]TProcessorFunction {
+	return map[string]TProcessorFunction{
+		"mock": WrappedTProcessorFunction{
+			Wrapped: func(ctx context.Context, seqId int32, in, out TProtocol) (bool, TException) {
+				return m.ProcessFunc(in, out)
+			},
+		},
+	}
+}
+
+func (m *mockProcessor) AddToProcessorMap(name string, processorFunc TProcessorFunction) {}
+
+type mockWrappedProcessorContextKey int
+
+const (
+	processorName mockWrappedProcessorContextKey = iota
+)
+
+// setMockWrappableProcessorName sets the "name" of the TProcessorFunction to
+// call on a mockWrappableProcessor when calling Process.
+//
+// In a normal TProcessor, the request name is read from the request itself
+// which happens in TProcessor.Process, so it is not passed into the call to
+// Process itself, to get around this in testing, mockWrappableProcessor calls
+// getMockWrappableProcessorName  to get the name to use from the context
+// object.
+func setMockWrappableProcessorName(ctx context.Context, name string) context.Context {
+	return context.WithValue(ctx, processorName, name)
+}
+
+// getMockWrappableProcessorName gets the "name" of the TProcessorFunction to
+// call on a mockWrappableProcessor when calling Process.
+func getMockWrappableProcessorName(ctx context.Context) (string, bool) {
+	val, ok := ctx.Value(processorName).(string)
+	return val, ok
+}
+
+// mockWrappableProcessor can be used to create a mock object that fufills the
+// TProcessor interface in testing.
+type mockWrappableProcessor struct {
+	ProcessorFuncs map[string]TProcessorFunction
+}
+
+// Process calls the TProcessorFunction assigned to the "name" set on the
+// context object by setMockWrappableProcessorName.
+//
+// If no name is set on the context or there is no TProcessorFunction mapped to
+// that name, the call will panic.
+func (p *mockWrappableProcessor) Process(ctx context.Context, in, out TProtocol) (bool, TException) {
+	name, ok := getMockWrappableProcessorName(ctx)
+	if !ok {
+		panic("MockWrappableProcessorName not set on context")
+	}
+	processor, ok := p.ProcessorMap()[name]
+	if !ok {
+		panic(fmt.Sprintf("No processor set for name %q", name))
+	}
+	return processor.Process(ctx, 0, in, out)
+}
+
+func (p *mockWrappableProcessor) ProcessorMap() map[string]TProcessorFunction {
+	return p.ProcessorFuncs
+}
+
+func (p *mockWrappableProcessor) AddToProcessorMap(name string, processorFunc TProcessorFunction) {
+	p.ProcessorFuncs[name] = processorFunc
+}
+
+var (
+	_ TProcessor = (*mockProcessor)(nil)
+	_ TProcessor = (*mockWrappableProcessor)(nil)
+)
