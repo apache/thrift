0503fc0112728bd17d9b5163716265c2a879404f
panivko <vladimir.panivko@together.com>
2020-05-29 08:17:16 +0300

    THRIFT-5132 Warning in TSocket when using ssl connection Client: PHP Patch: Vladimir Panivko
    This closes #2160

--- lib/php/lib/Transport/TSocket.php
+++ lib/php/lib/Transport/TSocket.php
@@ -251,8 +251,9 @@ class TSocket extends TTransport
         }
 
         if (function_exists('socket_import_stream') && function_exists('socket_set_option')) {
-            $socket = socket_import_stream($this->handle_);
-            socket_set_option($socket, SOL_TCP, TCP_NODELAY, 1);
+            // warnings silenced due to bug https://bugs.php.net/bug.php?id=70939
+            $socket = @socket_import_stream($this->handle_);
+            @socket_set_option($socket, SOL_TCP, TCP_NODELAY, 1);
         }
     }
 
