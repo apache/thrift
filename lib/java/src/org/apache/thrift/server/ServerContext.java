30da56bad198fe78c42c20cb1c8cf6a0b7ffef8d
David Mollitor <dmollitor@apache.org>
2021-02-03 16:58:17 -0500

    THRIFT-5345: Allow the ServerContext to be Unwrapped Programmatically

--- lib/java/src/org/apache/thrift/server/ServerContext.java
+++ lib/java/src/org/apache/thrift/server/ServerContext.java
@@ -18,9 +18,32 @@
  */
 
 /**
- * Interface for storing server's connection context
+ * Interface for storing server's connection context.
  */
- 
 package org.apache.thrift.server;
 
-public interface ServerContext {}
+public interface ServerContext {
+
+  /**
+   * Returns an object that implements the given interface to allow access to
+   * application specific contexts.
+   *
+   * @param iface A Class defining an interface that the result must implement
+   * @return an object that implements the interface
+   * @throws RuntimeException If the context cannot be unwrapped to the provided
+   *           class
+   */
+  <T> T unwrap(Class<T> iface);
+
+  /**
+   * Returns true if this server context is a wrapper for the provided
+   * application specific context interface argument or returns false otherwise.
+   *
+   * @param iface a Class defining the underlying context
+   * @return true if this implements the interface can be unwrapped to the
+   *         provided class
+   * @throws RuntimeException if an error occurs while determining whether the
+   *           provided class can be unwrapped from this context.
+   */
+  boolean isWrapperFor(Class<?> iface);
+}
