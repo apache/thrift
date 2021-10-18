apache/thrift

commit 91be87a76f747b9069bb07357483b2d8a19addcf

fix javadoc warnings Client: java Patch: Bhalchandra Pandit
This closes #2444

lib/java/src/org/apache/thrift/transport/TFileProcessor.java
@@ -45,7 +45,7 @@ public class TFileProcessor {
     outputTransport_ = outputTransport;
   }
 
-  public TFileProcessor(TProcessor processor, 
+  public TFileProcessor(TProcessor processor,
                         TProtocolFactory inputProtocolFactory,
                         TProtocolFactory outputProtocolFactory,
                         TFileTransport inputTransport,
@@ -67,7 +67,7 @@ public class TFileProcessor {
         processor_.process(ip, op);
         int newChunk = inputTransport_.getCurChunk();
         curChunk = newChunk;
-      } 
+      }
     } catch (TTransportException e) {
       // if we are processing the last chunk - we could have just hit EOF
       // on EOF - trap the error and stop processing.
@@ -84,6 +84,7 @@ public class TFileProcessor {
 
    * @param startChunkNum first chunk to be processed
    * @param endChunkNum last chunk to be processed
+   * @throws TException if endChunkNum < startChunkNum.
    */
   public void processChunk(int startChunkNum, int endChunkNum) throws TException {
     int numChunks = inputTransport_.getNumChunks();
@@ -104,6 +105,7 @@ public class TFileProcessor {
    * Process a single chunk
    *
    * @param chunkNum chunk to be processed
+   * @throws TException on error while processing the given chunk.
    */
   public void processChunk(int chunkNum) throws TException {
     processChunk(chunkNum, chunkNum);
@@ -111,6 +113,8 @@ public class TFileProcessor {
 
   /**
    * Process a current chunk
+   *
+   * @throws TException on error while processing the given chunk.
    */
   public void processChunk() throws TException {
     processChunk(inputTransport_.getCurChunk());

lib/java/src/org/apache/thrift/TDeserializer.java
@@ -43,7 +43,7 @@ public class TDeserializer {
   /**
    * Create a new TDeserializer that uses the TBinaryProtocol by default.
    *
-   * @throws TTransportException
+   * @throws TTransportException if there an error initializing the underlying transport.
    */
   public TDeserializer() throws TTransportException {
     this(new TBinaryProtocol.Factory());
@@ -54,7 +54,7 @@ public class TDeserializer {
    * factory that is passed in.
    *
    * @param protocolFactory Factory to create a protocol
-   * @throws TTransportException
+   * @throws TTransportException if there an error initializing the underlying transport.
    */
   public TDeserializer(TProtocolFactory protocolFactory) throws TTransportException {
     trans_ = new TMemoryInputTransport(new TConfiguration());

lib/java/src/org/apache/thrift/TSerializer.java
@@ -52,7 +52,7 @@ public class TSerializer {
   /**
    * Create a new TSerializer that uses the TBinaryProtocol by default.
    *
-   * @throws TTransportException
+   * @throws TTransportException if there an error initializing the underlying transport.
    */
   public TSerializer() throws TTransportException {
     this(new TBinaryProtocol.Factory());
@@ -63,7 +63,7 @@ public class TSerializer {
    * factory that is passed in.
    *
    * @param protocolFactory Factory to create a protocol
-   * @throws TTransportException
+   * @throws TTransportException if there an error initializing the underlying transport.
    */
   public TSerializer(TProtocolFactory protocolFactory) throws TTransportException {
     transport_ = new TIOStreamTransport(new TConfiguration(), baos_);

lib/java/src/org/apache/thrift/TUnion.java
@@ -168,7 +168,7 @@ public abstract class TUnion<T extends TUnion<T,F>, F extends TFieldIdEnum> impl
    * @param iprot input protocol from which to read a value.
    * @param field the field whose value is to be read from iprot.
    * @return read Object based on the field header, as specified by the argument.
-   * @throws TException
+   * @throws TException on error during read.
    */
   protected abstract Object standardSchemeReadValue(TProtocol iprot, TField field) throws TException;
   protected abstract void standardSchemeWriteValue(TProtocol oprot) throws TException;

lib/java/src/org/apache/thrift/transport/AutoExpandingBufferWriteTransport.java
@@ -31,6 +31,7 @@ public final class AutoExpandingBufferWriteTransport extends TEndpointTransport 
 
   /**
    * Constructor.
+   * @param config the configuration to use. Currently used for defining the maximum message size.
    * @param initialCapacity the initial capacity of the buffer
    * @param frontReserve space, if any, to reserve at the beginning such
    *                     that the first write is after this reserve.

lib/java/src/org/apache/thrift/transport/TFileTransport.java
@@ -407,6 +407,7 @@ public class TFileTransport extends TTransport {
    *
    * @param path File path to read and write from
    * @param readOnly Whether this is a read-only transport
+   * @throws IOException if there is an error accessing the file.
    */
   public TFileTransport(final String path, boolean readOnly) throws IOException {
     inputFile_ = new TStandardFile(path);

lib/java/src/org/apache/thrift/transport/TMemoryBuffer.java
@@ -34,6 +34,7 @@ public class TMemoryBuffer extends TEndpointTransport {
    * being written to it.
    *
    * @param size the initial size of the buffer
+   * @throws TTransportException on error initializing the underlying transport.
    */
   public TMemoryBuffer(int size) throws TTransportException {
     super(new TConfiguration());
@@ -46,8 +47,9 @@ public class TMemoryBuffer extends TEndpointTransport {
    * internal buffer will grow as necessary to accommodate the size of the data
    * being written to it.
    *
-   * @param config
+   * @param config the configuration to use.
    * @param size the initial size of the buffer
+   * @throws TTransportException on error initializing the underlying transport.
    */
   public TMemoryBuffer(TConfiguration config, int size) throws TTransportException {
     super(config);
@@ -121,4 +123,3 @@ public class TMemoryBuffer extends TEndpointTransport {
     return arr_.get();
   }
 }
-

lib/java/src/org/apache/thrift/transport/TNonblockingServerTransport.java
@@ -32,7 +32,7 @@ public abstract class TNonblockingServerTransport extends TServerTransport {
   /**
    *
    * @return an incoming connection or null if there is none.
-   * @throws TTransportException
+   * @throws TTransportException on error during this operation.
    */
   @Override
   public abstract TNonblockingTransport accept() throws TTransportException;

commit 2145741a9db80b2127e88ee5c792fe74d73b19dd

FIX for "error: malformed HTML" broken by 91be87a76f747b9069bb07357483b2d8a19addcf

lib/java/src/org/apache/thrift/transport/TFileProcessor.java
@@ -84,7 +84,7 @@ public class TFileProcessor {
 
    * @param startChunkNum first chunk to be processed
    * @param endChunkNum last chunk to be processed
-   * @throws TException if endChunkNum < startChunkNum.
+   * @throws TException if endChunkNum is less than startChunkNum.
    */
   public void processChunk(int startChunkNum, int endChunkNum) throws TException {
     int numChunks = inputTransport_.getNumChunks();