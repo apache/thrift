apache/thrift

commit 5f16344311a57ac016190984dd1aaa44e19a6487

fix wrong thrift strict header comparing

lib/rs/src/protocol/binary.rs
@@ -84,7 +84,7 @@ where
         // the thrift version header is intentionally negative
         // so the first check we'll do is see if the sign bit is set
         // and if so - assume it's the protocol-version header
-        if first_bytes[0] >= 8 {
+        if ((first_bytes[0] & 0x80) != 0) {
             // apparently we got a protocol-version header - check
             // it, and if it matches, read the rest of the fields
             if first_bytes[0..2] != [0x80, 0x01] {

commit 6f03b1d6b097025f333a5a2963da0ad53208f7e4

Fix travis issue caused by 5f16344311

lib/rs/src/protocol/binary.rs
@@ -84,7 +84,7 @@ where
         // the thrift version header is intentionally negative
         // so the first check we'll do is see if the sign bit is set
         // and if so - assume it's the protocol-version header
-        if ((first_bytes[0] & 0x80) != 0) {
+        if (first_bytes[0] & 0x80) != 0 {
             // apparently we got a protocol-version header - check
             // it, and if it matches, read the rest of the fields
             if first_bytes[0..2] != [0x80, 0x01] {