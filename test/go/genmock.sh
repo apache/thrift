19cf047cf22409e06b7e760725889cc0dafb1cea
Yuxuan 'fishy' Wang <yuxuan.wang@reddit.com>
2022-04-19 14:31:39 -0700

    Update Go versions used in travis and LANGUAGES.md
    Per our support policy, drop support of go 1.16 and add support of go
    1.18.
    
    Also enable go vet in make check, and fix issues reported by go vet.

--- test/go/genmock.sh
+++ test/go/genmock.sh
@@ -4,7 +4,7 @@ set -e
 
 export GOPATH=$(mktemp -d -t gopath-XXXXXXXXXX)
 
-GO111MODULE=on go install -mod=mod github.com/golang/mock/mockgen
+go install -mod=mod github.com/golang/mock/mockgen
 
 `go env GOPATH`/bin/mockgen -build_flags "-mod=mod" -destination=src/common/mock_handler.go -package=common github.com/apache/thrift/test/go/src/gen/thrifttest ThriftTest
 
