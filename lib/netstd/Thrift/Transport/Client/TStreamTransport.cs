c7d2e3b244f18b0030f305fa3f13c4d3047d5b29
Jens Geyer <jensg@apache.org>
2022-06-08 18:22:00 +0200

    FIX misc license headers

--- lib/netstd/Thrift/Transport/Client/TStreamTransport.cs
+++ lib/netstd/Thrift/Transport/Client/TStreamTransport.cs
@@ -14,6 +14,7 @@
 // KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations
 // under the License.
+
 using System;
 using System.IO;
 using System.Threading;
