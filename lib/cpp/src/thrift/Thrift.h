42204e7214461631b6f322aca0fc366080497aab
africamonkey <marses@vip.qq.com>
2022-10-31 21:27:38 +0800

    The != operator just call the == operator

--- lib/cpp/src/thrift/Thrift.h
+++ lib/cpp/src/thrift/Thrift.h
@@ -64,11 +64,7 @@ public:
     return (ii_ == rhs.ii_ && n_ == rhs.n_) || (is_end && is_rhs_end);
   }
 
-  bool operator!=(const TEnumIterator& rhs) const {
-    bool is_end = ii_ == n_ || n_ == -1;
-    bool is_rhs_end = rhs.ii_ == rhs.n_ || rhs.n_ == -1;
-    return (ii_ != rhs.ii_ || n_ != rhs.n_) && (!is_end || !is_rhs_end);
-  }
+  bool operator!=(const TEnumIterator& rhs) const { return !(*this == rhs); }
 
   std::pair<int, const char*> operator*() const { return std::make_pair(enums_[ii_], names_[ii_]); }
 
