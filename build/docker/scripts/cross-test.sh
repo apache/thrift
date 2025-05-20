9b85508f62072bcc81b2d55c21dbe0219df6cdf5
Carel Combrink <carel.combrink@gmail.com>
2025-05-20 08:20:36 +0200

    Disable a few options
    - Not interested in these now and looked more involved to solve
    - d: complains about some libss missing linker object
    - dart: complains about an sdk version
    - lisp: some compile error

--- build/docker/scripts/cross-test.sh
+++ build/docker/scripts/cross-test.sh
@@ -2,7 +2,7 @@
 set -ev
 
 ./bootstrap.sh
-./configure --enable-tutorial=no
+./configure --enable-tutorial=no --with-cl=no --with-d=no --with-dart=no
 make -j3 precross
 
 set +e
@@ -10,7 +10,7 @@ make cross$1
 
 RET=$?
 if [ $RET -ne 0 ]; then
-  if [ -f "test/features/log/unexpected_failures.log" ]; then 
+  if [ -f "test/features/log/unexpected_failures.log" ]; then
     cat "test/features/log/unexpected_failures.log"
   fi
   if [ -f "test/log/unexpected_failures.log" ]; then
