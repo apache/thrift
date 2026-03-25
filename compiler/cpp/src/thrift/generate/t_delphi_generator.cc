b4bf5167f4791ba048a8b0e44e16c436f36dd328
Jens Geyer <jensg@apache.org>
2026-03-25 23:47:30 +0000

    Remove throw to debug crash

--- compiler/cpp/src/thrift/generate/t_delphi_generator.cc
+++ compiler/cpp/src/thrift/generate/t_delphi_generator.cc
@@ -96,7 +96,7 @@ public:
       } else if( iter->first.compare("guid_v4") == 0) {
         guid_v4_ = true;
       } else {
-        throw std::string("FIXED: unknown delphi option: ") + iter->first;
+        // ignore unknown options for now
       }
     }
 
