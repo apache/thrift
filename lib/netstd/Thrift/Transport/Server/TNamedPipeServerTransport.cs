2702d3daf5bc2b7ca2426bbb8afca9d88d36b1b9
TheZaphodBeeblebrox <john.howard@ansys.com>
2026-07-08 09:14:03 -0400

    security fix: removed r/w access for all on named pipes

--- lib/netstd/Thrift/Transport/Server/TNamedPipeServerTransport.cs
+++ lib/netstd/Thrift/Transport/Server/TNamedPipeServerTransport.cs
@@ -189,7 +189,6 @@ namespace Thrift.Transport.Server
 
                     pipesec.SetOwner(sidOwner);
                     pipesec.AddAccessRule(new PipeAccessRule(sidOwner, PipeAccessRights.FullControl, AccessControlType.Allow));
-                    pipesec.AddAccessRule(new PipeAccessRule(sidWorld, PipeAccessRights.ReadWrite, AccessControlType.Allow));
                 }
 
                 // create a security descriptor and assign it to the security attribs
